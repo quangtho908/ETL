df1a4080410f00362e93ef61e9183acbc3f04508
quangtho908 <quangtho.dev@gmail.com>
2024-10-24 12:19:09 +0700

    update extract

--- src/extract/extract.module.ts
+++ src/extract/extract.module.ts
@@ -2,9 +2,13 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Config, ConfigSchema } from "src/schema/config.schema";
 import { ExtractService } from "./extract.service";
+import { Log, LogSchema } from "src/schema/log.schema";
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: Config.name, schema: ConfigSchema}])],
+  imports: [MongooseModule.forFeature([
+    {name: Config.name, schema: ConfigSchema},
+    {name: Log.name, schema: LogSchema}
+  ])],
   providers: [ExtractService],
   exports: [ExtractService]
 })
--- src/extract/extract.service.ts
+++ src/extract/extract.service.ts
@@ -26,20 +26,21 @@ export class ExtractService {
 
   async extract() {
     await this.getConfig();
-    // console.log(this.configs)
     for (let config of this.configs) {
-      const allProductLinks = await this.fetchLinks(config);
+      const products = await this.fetchLinks(config);
       const chunkSize = 10;
       const allProductsDetails = [];
-      for (let i = 0; i < allProductLinks.length; i += chunkSize) {
-        const chunk = allProductLinks.slice(i, i + chunkSize);
+      for (let i = 0; i < products.length; i += chunkSize) {
+        const chunk = products.slice(i, i + chunkSize);
         const productsDetails = await Promise.all(
-          chunk.map((link) => this.getProductDetails(link, config)),
+          chunk.map((product) => this.getProductDetails(product.link, config, {
+            name: product.name,
+            pricing: product.pricing
+          })),
         );
+
         allProductsDetails.push(...productsDetails);
       }
-
-      // const productsDetails = await Promise.all(allProductLinks.map(link => this.getProductDetails(link, config)));
 
       writeFileSync(
         config.file,
@@ -47,51 +48,27 @@ export class ExtractService {
         'utf-8',
       );
     }
-
-    // Chia mảng thành các nhóm và xử lý từng nhóm
-    // fs.writeFileSync('product_details.json', JSON.stringify(allProductsDetails, null, 2), 'utf-8');
-    // console.log('Product details saved to product_details.json');
-    // Hiển thị thông tin sản phẩm
-    // allProductsDetails.forEach((details, index) => {
-    //     console.log(`Details for product ${index + 1}:`, details);
-    // });
   }
 
-  async getProductDetails(link, config: Config) {
+  async getProductDetails(link, config: Config, product) {
     const { data } = await axios.get(link);
     const $ = cheerio.load(data);
     const details = {};
     const query = JSON.parse(config.query);
+    const mapKey = JSON.parse(config.mapExtract);
     query.forEach((q) => {
       if (q.type === 'common') {
         $(q.parent).each((_index, element) => {
           const name = $(element).find(q.name).text().trim();
           let value = $(element).find(q.value).text().trim();
           value = value.replace(/\n\s{52}/g, '_');
-          details[name] = value;
+          details[mapKey[name]] = value;
         });
         return;
       }
       details[q.name] = $(q.value).text().trim();
     });
-
-    // Lấy tên sản phẩm từ thẻ h1
-    // const productName = $('h1').text().trim();
-    // details["Tên sản phẩm"] = productName;
-
-    // Lấy giá từ thẻ .box-price-present
-    // const productPrice = $('.box-price-present').text().trim();
-    // details["Giá"] = productPrice;
-
-    // Duyệt qua từng phần tử <li> để lấy tên và giá trị
-    // $('.text-specifi li').each((index, element) => {
-    //     const name = $(element).find('aside:first-child').text().trim();
-    //     let value = $(element).find('aside:last-child').text().trim();
-    //     value = value.replace(/\n\s{52}/g, '_');
-    //     details[name] = value;
-    // });
-    // console.log(details)
-    return details;
+    return {...details, ...product};
   }
 
   async fetchLinks(config: Config) {
@@ -99,6 +76,7 @@ export class ExtractService {
     const headers = JSON.parse(config.headers);
     let pageIndex = 0;
     let links = [];
+    const result = [];
     while (true) {
       try {
         const response = await axios({
@@ -118,12 +96,21 @@ export class ExtractService {
           break;
         }
         const $ = cheerio.load(html);
-        $(`${config.queryUrlDetail}`).each((index, element) => {
+        const patch: {link: string, name: string, pricing: string} = {
+          link: '',
+          name: '',
+          pricing: ''
+        }
+        $(config.queryUrlDetail).each((_index, element) => {
           const href = $(element).attr('href');
-          if (href) {
-            links.push(`${config.url}${href}`);
-          }
+          if(href) {
+            patch.name = $(element).find(config.queryName).text().trim();
+            patch.pricing = $(element).find(config.queryPricing).text().trim();
+            patch.link = `${config.url}${href}`
+            result.push(patch)
+          };
         });
+
         pageIndex++;
       } catch (error) {
         await this.logEvent(
@@ -136,7 +123,7 @@ export class ExtractService {
         break;
       }
     }
-    return links;
+    return result
   }
 
   async logEvent(
