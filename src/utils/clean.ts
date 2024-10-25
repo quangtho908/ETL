f93e91bc7dc4d728d1cbda7eec180872080bebd1
quangtho908 <quangtho.dev@gmail.com>
2024-10-25 16:44:56 +0700

    add factory clean

--- src/utils/clean.ts
+++ src/utils/clean.ts
@@ -1,7 +1,7 @@
 export const cleanAction = {
   replace (product, attribute, data) {
+    if(product[attribute] === null || !data?.regex) return product[attribute] || ""
     const regex = new RegExp(data.text, "g")
-    if(product[attribute] === null) return ""
     return product[attribute].replace(regex, data.value)
   },
 
@@ -11,11 +11,11 @@ export const cleanAction = {
         return data.value
       }
     }
-    return product[attribute]
+    return product[attribute] || ""
   },
 
   fillMissing(product, attribute, data) {
-    if(!!product[attribute] || !data?.range[0]) return product[attribute];
+    if(!!product[attribute] || !data?.range[0]) return product[attribute] || "";
     const array = (product[data.depend] || "").split(" ")
     return array.slice(data.range[0], data.range[1]).join(" ")
   }
