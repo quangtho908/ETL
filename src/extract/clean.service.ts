ebda8d63dd562b311baae9f3cb000a4faacc1e62
iamtankiet <20130299@st.hcmuaf.edu.vn>
2024-10-28 21:58:52 +0700

    fix clean

--- src/extract/clean.service.ts
+++ src/extract/clean.service.ts
@@ -1,35 +1,40 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { InjectRepository } from "@nestjs/typeorm";
-import { attr } from "cheerio/dist/commonjs/api/attributes";
-import { Model } from "mongoose";
-import { Staging } from "src/entities/staging.entity";
-import { Config } from "src/schema/config.schema";
-import { Log } from "src/schema/log.schema";
-import { cleanAction } from "src/utils";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Model } from 'mongoose';
+import { Staging } from 'src/entities/staging.entity';
+import { Config } from 'src/schema/config.schema';
+import { Log } from 'src/schema/log.schema';
+import { cleanAction } from 'src/utils';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CleanService {
   constructor(
     @InjectModel(Config.name) private configModel: Model<Config>,
     @InjectModel(Log.name) private logModel: Model<Log>,
-    @InjectRepository(Staging) private stagingRepo: Repository<Staging>
+    @InjectRepository(Staging) private stagingRepo: Repository<Staging>,
   ) {}
 
   async clean(id: string) {
-    const config = await this.configModel.findById(id)
-    const cleanOptions = JSON.parse(config.cleanOptions)
-    const products = await this.stagingRepo.find({})
+    const config = await this.configModel.findById(id);
+    const cleanOptions = JSON.parse(config.cleanOptions);
+    const products = await this.stagingRepo.find({});
     products.forEach((product) => {
-      cleanOptions.forEach(cleanOpt => {
-        for(let action in cleanOpt.action) {
-          const newData = cleanAction[action](product, cleanOpt.attr, cleanOpt.action[action])
-          product[cleanOpt.attr] = newData.trim()
-        }
-      })
-    })
+      cleanOptions.forEach(
+        (cleanOpt: { action: { [x: string]: any }; attr: string | number }) => {
+          for (const action in cleanOpt.action) {
+            const newData = cleanAction[action](
+              product,
+              cleanOpt.attr,
+              cleanOpt.action[action],
+            );
+            product[cleanOpt.attr] = newData.trim();
+          }
+        },
+      );
+    });
 
-    this.stagingRepo.save(products)
+    await this.stagingRepo.save(products);
   }
 }
--- src/utils/clean.ts
+++ src/utils/clean.ts
@@ -1,22 +1,40 @@
+import * as _ from 'lodash';
+
 export const cleanAction = {
-  replace (product, attribute, data) {
-    if(product[attribute] === null || !data?.regex) return product[attribute] || ""
-    const regex = new RegExp(data.text, "g")
-    return product[attribute].replace(regex, data.value)
+  replace(
+    product: { [x: string]: string },
+    attribute: string | number,
+    data: { regex: any; text: string | RegExp; value: any },
+  ) {
+    if (product[attribute] === null || !!data?.regex)
+      return product[attribute] || '';
+    const regex = new RegExp(data.text, 'g');
+    return product[attribute].replace(regex, data.value);
   },
 
-  replaceBy(product, attribute, data) {
-    for(let contain of data.contains) {
-      if(product[attribute].toLowerCase().indexOf(contain.toLowerCase()) >= 0) {
-        return data.value
+  replaceBy(
+    product: { [x: string]: any },
+    attribute: string | number,
+    data: { contains: any; value: any },
+  ) {
+    for (const contain of data.contains) {
+      if (
+        product[attribute].toLowerCase().indexOf(contain.toLowerCase()) >= 0
+      ) {
+        return data.value;
       }
     }
-    return product[attribute] || ""
+    return product[attribute] || '';
   },
 
-  fillMissing(product, attribute, data) {
-    if(!!product[attribute] || !data?.range[0]) return product[attribute] || "";
-    const array = (product[data.depend] || "").split(" ")
-    return array.slice(data.range[0], data.range[1]).join(" ")
-  }
-}
+  fillMissing(
+    product: { [x: string]: any },
+    attribute: string | number,
+    data: { range: any[]; depend: string | number },
+  ) {
+    if (_.isEmpty(data?.range)) return product[attribute] || '';
+
+    const array = (product[data.depend] || '').split(' ');
+    return array.slice(data.range[0], data.range[1]).join(' ');
+  },
+};
